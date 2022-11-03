5a09cd359ec2f4fa472811dc08052c5e085e22d2
AdarshSrikanth <31250365+AdarshSrikanth@users.noreply.github.com>
2022-11-02 18:42:51 -0700

    DP - Word Break

---
+++ src/main/algo/dp/WordBrak.java
@@ -0,0 +1,33 @@
+package main.algo.dp;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class WordBrak {
+    public static void main(String[] args) {
+        List<String> words = new ArrayList<String> ();
+        words.add("aaaa");
+        words.add("aaa");
+        wordBreak("aaaaaaa", words);
+    }
+    
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < wordDict.size(); j++) {
+                String subs = wordDict.get(j);
+                if (i + subs.length() > n) {
+                    continue;
+                }
+                String ssub = s.substring(i, i + subs.length());
+                if (subs.equals(ssub)) {
+                    dp[i + subs.length()] = dp[i + subs.length()] || dp[i];
+                }
+            }
+        }
+        return dp[n];
+    }
+}
