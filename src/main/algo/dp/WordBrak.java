3be1589da21e11beb3f97679757647be3798b95a
AdarshSrikanth <31250365+AdarshSrikanth@users.noreply.github.com>
2022-11-02 18:45:00 -0700

    DP - Word Break

--- src/main/algo/dp/WordBrak.java
+++ src/main/algo/dp/WordBrak.java
@@ -12,6 +12,15 @@ public class WordBrak {
         wordBreak("aaaaaaa", words);
     }
     
+    /**
+     * https://leetcode.com/problems/word-break/submissions
+     * 
+     * O(n^2) run time and O(n) space
+     * 
+     * @param s
+     * @param wordDict
+     * @return
+     */
     public static boolean wordBreak(String s, List<String> wordDict) {
         int n = s.length();
         boolean[] dp = new boolean[n + 1];
@@ -25,6 +34,8 @@ public class WordBrak {
                 String ssub = s.substring(i, i + subs.length());
                 if (subs.equals(ssub)) {
                     dp[i + subs.length()] = dp[i + subs.length()] || dp[i];
+                    // initially dp[i + subs.length()] is false. If we had found any match in the past, then
+                    // make sure this never becomes false as dp[i] for current word might be false;
                 }
             }
         }
