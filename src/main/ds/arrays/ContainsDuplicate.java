25ddd5913713d37215d88aeab3189bd5f080ed60
Adarsh Srikanth <adarsh.srikanth@gmail.com>
2023-10-14 00:11:28 -0700

    Check if an array contains duplicate element

---
+++ src/main/ds/arrays/ContainsDuplicate.java
@@ -0,0 +1,37 @@
+package main.ds.arrays;
+
+import main.Util;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+
+/* https://leetcode.com/problems/contains-duplicate/ */
+public class ContainsDuplicate {
+    public static void main (String[] args) {
+        int[] nums = Util.get_input_array(new Scanner(System.in));
+        System.out.println("\nInput array contains duplicates: " + containsDuplicate(nums));
+    }
+
+    // Time: O(nlogn) and Memory: O(n)
+    public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i+1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Time: O(n) and Memory: O(2n)
+    public static boolean containsDuplicate2(int[] nums) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.add(nums[i])) {
+                return true; 
+            }
+        }
+        return false;
+    }
+}
